6fc31c36a594f77be7a62b37a97df711f665144a
uvsohith2004 <uvsohith2004@gmail.com>
2025-08-27 23:28:27 +0530

    minner changes in code

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://visvotsav.vercel.app", "http://localhost:5173"],
+    origin: ["visvotsav-teal.vercel.app"],
     credentials: true,
   })
 );
--- server/src/config/sheet-config.js
+++ server/src/config/sheet-config.js
@@ -2,7 +2,10 @@ import { google } from 'googleapis'
 import dotenv from 'dotenv'
 dotenv.config();
 export const auth = new google.auth.GoogleAuth({
-  keyFile: 'credentials.json',
+  credentials: {
+    client_email: process.env.CLIENT_EMAIL,
+    private_key: process.env.PRIVATE_KEY,
+  },
   scopes: 'https://www.googleapis.com/auth/spreadsheets',
 });
 const client = await auth.getClient();
