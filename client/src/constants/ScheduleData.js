4b9d05e0761aa1d04f9cb4442d2b263f40b557cd
uvsohith2004 <uvsohith2004@gmail.com>
2025-08-29 16:18:22 +0530

    Major Changes

--- client/src/constants/ScheduleData.js
+++ client/src/constants/ScheduleData.js
@@ -15,115 +15,37 @@ export const scheduleData = [
     },
     {
       id: 3,
-      title: "Technical Expo",
-      date: "14th September 2025",
-      time: "10:00 AM",
-      venue: "Expo Center"
-    },
-    {
-      id: 4,
       title: "Coding",
       date: "14th September 2025",
       time: "10:00 AM",
       venue: "Computer Lab"
     },
     {
-      id: 5,
+      id: 4,
       title: "Circuitrix",
       date: "14th September 2025",
       time: "10:00 AM",
       venue: "Electronics Lab"
     },
     {
-      id: 6,
+      id: 5,
       title: "Project Expo",
       date: "14th September 2025",
       time: "10:00 AM",
       venue: "Main Hall"
     },
     {
-      id: 7,
+      id: 6,
       title: "Web Design",
       date: "14th September 2025",
       time: "10:00 AM",
       venue: "Main Hall"
     },
     {
-      id: 8,
-      title: "Basket Ball",
+      id: 7,
+      title: "Technical Quiz",
       date: "15th September 2025",
       time: "10:00 AM",
       venue: "Sports Complex"
     },
-    {
-      id: 9,
-      title: "Volleyball",
-      date: "15th September 2025",
-      time: "2:00 PM",
-      venue: "Sports Complex"
-    },
-    {
-      id: 10,
-      title: "Badminton",
-      date: "15th September 2025",
-      time: "4:00 PM",
-      venue: "Sports Complex"
-    },
-    {
-      id: 11,
-      title: "Throwball",
-      date: "15th September 2025",
-      time: "6:00 PM",
-      venue: "Sports Complex"
-    },
-    {
-      id: 12,
-      title: "Chess",
-      date: "15th September 2025",
-      time: "10:00 AM",
-      venue: "Indoor Games Room"
-    },
-    {
-      id:13,
-      title: "Singing",
-      date: "15th September 2025",
-      time: "11:00 AM",
-      venue: "Auditorium"
-    },
-    {
-      id:14,
-      title: "Dancing",
-      date: "15th September 2025",
-      time: "12:00 PM",
-      venue: "Dance Hall"
-    },
-    {
-      id: 15,
-      title: "Drawing",
-      date: "15th September 2025",
-      time: "01:00 PM",
-      venue: "Art Room"
-    },
-    {
-      id: 16,
-      title: "Dumb Charades",
-      date: "15th September 2025",
-      time: "02:00 PM",
-      venue: "Drama Room"
-    },
-    {
-      id: 17,
-      title: "Musical Instruments",
-      date: "15th September 2025",
-      time: "03:00 PM",
-      venue: "Music Room"
-    },
-    {
-      id:18,
-      title: "Antyakshari",
-      date: "15st September 2025",
-      time: "04:00 PM",
-      venue: "Cultural Hall"
-    }
-    
   ];
