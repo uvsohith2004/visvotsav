5f981bd6e621ad7985c892c34e1cbe1d0c25d619
Masani Yasovardhan <yasovardhanmasani@gmail.com>
2025-09-09 22:41:50 +0530

    fix: add headers to cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,10 +8,10 @@ async function bootstrap() {
 const app = await NestFactory.create(AppModule);
 
 app.enableCors({
-  origin: ["http://localhost:5173", "https://visvotsav.pbrvits.ac.in","https://visvotsav-teal.vercel.app"],
+  origin: "*",
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', 
   credentials: true,
-  allowedHeaders: 'Content-Type, Accept, Authorization',
+  allowedHeaders: 'Content-Type, Authorization',
 });
 app.useGlobalPipes(new ValidationPipe());
 const config = new DocumentBuilder()
