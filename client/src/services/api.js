1194e568ec015fe5d8a1620821d26b123ec5dd4c
Masani Yasovardhan <yasovardhanmasani@gmail.com>
2025-09-10 21:34:12 +0530

    chore:verfying base url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:4000"; 
 
+console.log(BASE_URL)
 const axiosInstance = axios.create({ baseURL: BASE_URL });
 
 export const postQuery = async (data) => {
